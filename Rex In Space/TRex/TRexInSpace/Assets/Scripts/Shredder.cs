70da1dbe1b3a327f5dd1b0505751f727ea6624bc
adberry <adamberry295@yahoo.com>
2017-03-29 14:14:25 -0500

    Added local files to repo
    This is the initial commit with the completed project from eHacks17.

---
+++ Rex In Space/TRex/TRexInSpace/Assets/Scripts/Shredder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour {
+    
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    Destroy(collision.gameObject);
+    //}
+
+    //void OnCollisionEnter(Collider other)
+    //{
+    //    if (other.gameObject.tag == "Player")
+    //        Destroy(this.gameObject);
+    //}
+}
